d743ad5a7fbdc1f4863cf8546d5329c2b38e4e82
Kaushal Kaslikar <kaushal.kaslikar@gmail.com>
2021-01-10 21:13:52 +0800

    Changes to checking_chromosome_number.R

--- checking_chromosome_number.R
+++ checking_chromosome_number.R
@@ -3,13 +3,21 @@ library(tidyverse)
 
 # Checking number of the chromosomes that reads aligned to ----------------
 
+## nfields <- system("awk -F'\t' '{print NF}' Tgfbr1_probes.sam", intern = TRUE)
+
+## nfields
+
+## max(nfields)
+
 sam <- read_tsv("Tgfbr1_probes.sam", col_names = FALSE) # file containing potential multimappers, with chromosome location data
  
 bed <- read_tsv("Tgfbr1_cleaned_probes.bed", col_names = FALSE) # file cleaned of all multimappers, with no chromosome data
 
+bed
+
 colnames(bed) <- c("chromosome", "start", "end", "probe", "Tm")
 
-sam[which(sam$X10 %in% bed$probe), ] %>% select(X3) %>% distinct() # finding the chromosome number of probes in bed
+sam[which(sam$X10 %in% bed$probe), ] %>% select(X3) %>% distinct() # finding the chromosome ID of the uniquely-mapping probes in bed
 
 
 
@@ -17,16 +25,19 @@ sam[which(sam$X10 %in% bed$probe), ] %>% select(X3) %>% distinct() # finding the
 
 bed$chromosome <- str_replace(bed$chromosome, pattern = "chrom", replacement = "chr4") # adding the relevant chromosome name
 
+bed
+
 # Modifying co-ordinates to genomic co-ordinates for chromosome 4 ---------
 
 tgfbr1_bounds <- c("start" = 47353222, "end" = 47414926) # co-ordinates for Tgfbr1 on chromosome 4
 
-bed_align <- bed
+bed_align <- bed   # bed with new co-ordinates
 
 bed_align$start <- (bed_align$start - 1 + tgfbr1_bounds["start"]) # adjusting the start positions
 
 bed_align$end <- (bed_align$end - 1 + tgfbr1_bounds["start"]) # adjusting the end positions
 
+bed_align
 
 # Filtering for consistent Tm ---------------------------------------------
 
@@ -41,7 +52,6 @@ bed_align <- bed_align %>% filter(Tm <= 57)
 write_tsv(x = bed_align, path = "Tgfbr1_cleaned_probes_chr.bed", col_names = FALSE)
 
 # tgfbr1_range <- GRanges(seqnames = c("chr4"), ranges = IRanges (start = tgfbr1_bounds["start"], end = tgfbr1_bounds["end"]))
-
 
 # probe_ranges <- IRanges(start = bed$start, end = bed$end)
 
@@ -59,29 +69,38 @@ write_tsv(x = bed_align, path = "Tgfbr1_cleaned_probes_chr.bed", col_names = FAL
 
 # Checking whether co-ordinates from outputClean are w.r.t. Tgfbr1 --------
 
-library(tidyverse)
-library(GenomicRanges)
-library(Biostrings)
+## library(tidyverse)
+## library(GenomicRanges)
+## library(Biostrings)
 
-sequence <- readDNAStringSet("Tgfbr1.fa")
+## sequence <- readDNAStringSet("Tgfbr1.fa")
 
-sequence <- sequence[[1]]
+## sequence
 
-probes <- DNAStringSet(x = bed_align$probe)
+## sequence <- sequence[[1]]
 
-al <- list()
+## sequence
 
-for(i in seq_along(probes)){
- al[i] <- matchPattern(pattern = probes[[i]], subject = sequence)
-}
+## probes <- DNAStringSet(x = bed_align$probe)
 
-al <- map(.x = al, .f = ~as(object = .x, Class = "IRanges"))
+## al <- lapply(seq_along(probes), FUN = function(i){
+  ## matchPattern(pattern = probes[[i]], subject = sequence)
+## })
 
-al <- IRangesList(al)
+## al
 
-al <- unlist(al)
+## al <- map(.x = al, .f = ~as(object = .x, Class = "IRanges"))
 
-al + tgfbr1_bounds["start"]
+## al
 
-al <- IRanges(start = (start(al) + tgfbr1_bounds["start"] - 1), end = (end(al) + tgfbr1_bounds["start"] - 1))
+## al <- IRangesList(al)
 
+## al <- unlist(al)
+
+## al
+
+## al <- IRanges(start = (start(al) + tgfbr1_bounds["start"] - 1), end = (end(al) + tgfbr1_bounds["start"] - 1))
+
+## all(start(al) == bed_align$start)
+
+## all(end(al) == bed_align$end)
